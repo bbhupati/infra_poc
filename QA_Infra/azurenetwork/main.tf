0f122b8631e9e45476ebe009b7ae2cdbbcf4bbde
Brahmaiah <bhupathi.brahmaiah@arcadis.com>
2023-03-29 11:55:19 +0530

    initial commit

---
+++ QA_Infra/azurenetwork/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
